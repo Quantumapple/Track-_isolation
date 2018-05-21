4355a00ff7b410d487dbaa539549d42ba689255c
Quantumapple <jongho.lee@cern.ch>
2018-05-22 00:30:14 +0900

    Find points_v2

--- roofit_input.C
+++ roofit_input.C
@@ -31,9 +31,10 @@ void roofit_input::Loop()
    
    Int_t nbins = 150; Float_t x1 = 0.; Float_t x2 = 150.;
    TH1F* hist_bsl1_l1l2_a = new TH1F("hist_bsl1_l1l2_a",";P_{T} (GeV); #Delta#phi", 18, 0.2, 2.0);
-   TH1F* hist_bsl1_l1l2_b = new TH1F("hist_bsl1_l1l2_b",";P_{T} (GeV); #Delta#phi", 23, 2.0, 25.);
-   TH1F* hist_bsl1_l1l2_c = new TH1F("hist_bsl1_l1l2_c",";P_{T} (GeV); #Delta#phi", 10, 25., 50.);
-   TH1F* hist_bsl1_l1l2_d = new TH1F("hist_bsl1_l1l2_d",";P_{T} (GeV); #Delta#phi", 20, 50., 150.);
+   TH1F* hist_bsl1_l1l2_b = new TH1F("hist_bsl1_l1l2_b",";P_{T} (GeV); #Delta#phi", 6, 2.0, 5.0);
+   TH1F* hist_bsl1_l1l2_c = new TH1F("hist_bsl1_l1l2_c",";P_{T} (GeV); #Delta#phi", 20, 5.0, 25.);
+   TH1F* hist_bsl1_l1l2_d = new TH1F("hist_bsl1_l1l2_d",";P_{T} (GeV); #Delta#phi", 10, 25., 50.);
+   TH1F* hist_bsl1_l1l2_e = new TH1F("hist_bsl1_l1l2_e",";P_{T} (GeV); #Delta#phi", 20, 50., 150.);
 
    ofstream Bsl1_l1l2_points;
    Bsl1_l1l2_points.open("./Bsl1_l1l2_points.h");
@@ -42,8 +43,8 @@ void roofit_input::Loop()
    Bsl1_l1l2_points << "#define Bsl1_l1l2_points_h" << endl;
    Bsl1_l1l2_points << endl;
 
-   Bsl1_l1l2_points << "double gen_Pt[70] ={}, point[70] = {}; " << endl;
-   Bsl1_l1l2_points << "int points = 70;" << endl;
+   Bsl1_l1l2_points << "double gen_Pt[74] ={}, point[74] = {}, error[74] = {};" << endl;
+   Bsl1_l1l2_points << "int points = 74;" << endl;
 
    Bsl1_l1l2_points << "void set_arrary(){" << endl;
    Bsl1_l1l2_points << endl;
@@ -62,7 +63,7 @@ void roofit_input::Loop()
 
    for(Int_t nth = 2; nth < 4; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -102,7 +103,7 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.01);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
        hist_bsl1_l1l2_a->SetBinContent(count+1, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
@@ -128,7 +129,7 @@ void roofit_input::Loop()
    
    for(Int_t nth = 4; nth < 10; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -168,7 +169,7 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.001);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
        hist_bsl1_l1l2_a->SetBinContent(count+1, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
@@ -194,7 +195,7 @@ void roofit_input::Loop()
   
    for(Int_t nth = 10; nth < 20; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -234,7 +235,7 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.001);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
        hist_bsl1_l1l2_a->SetBinContent(count+1, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
@@ -258,9 +259,76 @@ void roofit_input::Loop()
 
    } // end of nth loop ( 1 ~ 2 GeV )
 
-   for(Int_t nth = 2; nth < 25; nth++)
+   for(Int_t nth = 0; nth < 6; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
+       TString file_ = "./Fix_results/phi_";
+       TString ith_;
+
+       ith_.Form("%d", count+1);
+       file_ = file_ + ith_ + "th.pdf";
+
+       low_pt = 2. + nth*0.5;
+       high_pt = low_pt + 0.5;
+
+       cout << "Low pT bound: " << low_pt << ", high pT bound: " << high_pt << endl;
+
+       sprintf(histname, "hist_%dth", count+1);
+       H[count] = new TH1F(histname,"",250,-0.04,0.02);
+       
+       for (Long64_t jentry=0; jentry<nentries;jentry++) {
+	   Long64_t ientry = LoadTree(jentry);
+	   if (ientry < 0) break;
+	   nb = fChain->GetEntry(jentry);   nbytes += nb;
+	   // if (Cut(ientry) < 0) continue;
+
+	   int size = bsl1_l1l2_dphi->size();
+	   int pT_size = genpT->size(); 
+
+	   if( size == 0 ) continue;
+
+	   //cout << "Size of dphi vector: " << size << ", size of pT vector: " << pT_size << endl;
+	   //cout << "dPhi: " << bsl1_l1l2_dphi->at(0) << ", pT: " << genpT->at(0) << endl;
+
+           Float_t gen_pT = genpT->at(0);
+	   if( gen_pT > high_pt || gen_pT < low_pt ) continue;
+	   //cout << "generator level pT: " << gen_pT << endl;
+
+	   H[count]->Fill(bsl1_l1l2_dphi->at(0));
+
+       }// end of event loop
+       cout << endl;
+
+       func->SetParameters(70, H[count]->GetMean(), 0.001);
+       H[count]->Fit(func,"0R");
+       cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
+       hist_bsl1_l1l2_b->SetBinContent(nth+1, fabs(func->GetParameter(1)));
+       gStyle->SetOptFit(1111);
+
+       func->SetLineColor(kRed);
+       func->SetLineWidth(2);
+
+       TCanvas* c1 = new TCanvas("c1","",800,800) ;
+       c1->cd(1); 
+       gPad->SetLeftMargin(0.15);  
+       H[count]->Draw();
+       func->Draw("same");
+
+       c1->SaveAs(file_);
+       c1->Write();
+
+       c1->Clear();
+       delete c1;
+  
+       count++;
+       cout << endl; 
+
+   } // end of nth loop ( 2 ~ 5 GeV )
+   
+   for(Int_t nth = 5; nth < 25; nth++)
+   {
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -301,8 +369,8 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.001);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
-       hist_bsl1_l1l2_b->SetBinContent(nth-1, fabs(func->GetParameter(1)));
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
+       hist_bsl1_l1l2_c->SetBinContent(nth-4, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
        func->SetLineColor(kRed);
@@ -323,12 +391,12 @@ void roofit_input::Loop()
        count++;
        cout << endl; 
 
-   } // end of nth loop ( 2 ~ 25 GeV )
+   } // end of nth loop ( 5 ~ 25 GeV )
   
    
    for(Int_t nth = 0; nth < 10; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -370,8 +438,8 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.001);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
-       hist_bsl1_l1l2_c->SetBinContent(nth+1, fabs(func->GetParameter(1)));
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
+       hist_bsl1_l1l2_d->SetBinContent(nth+1, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
        func->SetLineColor(kRed);
@@ -396,7 +464,7 @@ void roofit_input::Loop()
 
    for(Int_t nth = 0; nth < 20; nth++)
    {
-       cout << "Process: (" << count+1 << "/70)" << endl;
+       cout << "Process: (" << count+1 << "/74)" << endl;
        TString file_ = "./Fix_results/phi_";
        TString ith_;
 
@@ -438,8 +506,8 @@ void roofit_input::Loop()
        func->SetParameters(70, H[count]->GetMean(), 0.001);
        H[count]->Fit(func,"0R");
        cout << "Fit parameters: " << "Const: " << func->GetParameter(0) << ", Mean: " << func->GetParameter(1) << ", sigma: " << func->GetParameter(2) << endl;
-       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << ";" << endl;
-       hist_bsl1_l1l2_d->SetBinContent(nth+1, fabs(func->GetParameter(1)));
+       Bsl1_l1l2_points << "gen_Pt[" << count << "] = " << low_pt << "; point[" << count << "] = " << fabs(func->GetParameter(1)) << "; error[" << count << "] = " << fabs(func->GetParameter(2)) << ";" << endl;
+       hist_bsl1_l1l2_e->SetBinContent(nth+1, fabs(func->GetParameter(1)));
        gStyle->SetOptFit(1111);
 
        func->SetLineColor(kRed);
@@ -507,6 +575,11 @@ void roofit_input::Loop()
    hist_bsl1_l1l2_d->SetMarkerStyle(20);
    hist_bsl1_l1l2_d->SetMarkerSize(1.);
    hist_bsl1_l1l2_d->Draw("hist p same");
+   
+   hist_bsl1_l1l2_e->SetMarkerColor(1);
+   hist_bsl1_l1l2_e->SetMarkerStyle(20);
+   hist_bsl1_l1l2_e->SetMarkerSize(1.);
+   hist_bsl1_l1l2_e->Draw("hist p same");
 
    Bsl1_l1l2_points << "}" << endl;
    Bsl1_l1l2_points << "#endif" << endl;
@@ -517,6 +590,7 @@ void roofit_input::Loop()
    hist_bsl1_l1l2_b->Write();
    hist_bsl1_l1l2_c->Write();
    hist_bsl1_l1l2_d->Write();
+   hist_bsl1_l1l2_e->Write();
    c2->Print("Fix_results/Bsl1_l1l2_dphi.pdf");
    
 
