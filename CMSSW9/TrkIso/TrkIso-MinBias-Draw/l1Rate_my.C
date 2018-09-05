e1a3210cbed1299e56480e034fcad73ab56d3ce7
Quantumapple <jongho.lee@cern.ch>
2018-09-05 23:25:51 +0900

    Draw Minbias plot

---
+++ CMSSW9/TrkIso/TrkIso-MinBias-Draw/l1Rate_my.C
@@ -0,0 +1,269 @@
+#define l1Rate_my_cxx
+#include "l1Rate_my.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+
+void l1Rate_my::Loop()
+{
+   if (fChain == 0) return;
+
+   Long64_t nentries = fChain->GetEntriesFast();
+   
+   float genN = 496179.0; 
+   int eta_r = 1;
+
+   int nbins = 151; float x1 = 9. ; float x2 = 160. ;
+   TH1F* hEG = new TH1F("hEG",";E_{T} threshold (GeV); Rate (kHz)",nbins,x1,x2);
+   TH1F* hPXEG = new TH1F("hPXEG",";E_{T} threshold (GeV); Rate (kHz)",nbins,x1,x2);
+   TH1F* hPXIsoEG = new TH1F("hPXIsoEG",";E_{T} threshold (GeV); Rate (kHz)",nbins,x1,x2);
+
+   TH1F* r_PXEG = new TH1F("L1PXEG_r_factor",";E_{T} threshold (GeV); Rate reduction factor",nbins,x1,x2);
+   TH1F* r_PXIsoEG = new TH1F("L1PXIsoEG_r_factor",";E_{T} threshold (GeV); Rate reduction factor",nbins,x1,x2);
+
+   int width_bit[27] = {0x1, 0x2, 0x4, 0x8, 0x10, 0x20, 0x40, 0x80, 0x100, 0x200, 0x400, 0x800, 0x1000, 0x2000, 0x4000, 0x8000, 0x10000, 0x20000, 0x40000, 0x80000,
+                        0x100000, 0x200000, 0x400000, 0x800000, 0x1000000, 0x2000000, 0x4000000};
+
+   Long64_t nbytes = 0, nb = 0;
+   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+      Long64_t ientry = LoadTree(jentry);
+      if (ientry < 0) break;
+      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      // if (Cut(ientry) < 0) continue;
+   
+      if(!ntnEg2) continue;
+
+      float max_eget = -999.;
+      for(int i=0; i < ntnEg2; i++){
+
+         if(eta_r == 0 && ntEgEt->at(i) > max_eget && (fabs(ntEgEta->at(i)) < 3.) ) max_eget = ntEgEt->at(i);
+         //if(eta_r == 1 && ntEgEt->at(i) > max_eget && (fabs(ntEgEta->at(i)) < 1.3) ) max_eget = ntEgEt->at(i);
+         if(eta_r == 1 && ntEgEt->at(i) > max_eget && (fabs(ntEgEta->at(i)) < 0.8) ) max_eget = ntEgEt->at(i);
+           
+      }// eg loop
+
+      float max_pxeget = -999.;
+      for(int i=0; i < ntnEg2; i++){
+         if(eta_r == 0 && ntEgEt->at(i) > max_pxeget && ((trigger_bit_width->at(i)&width_bit[0])==width_bit[0]) && (fabs(ntEgEta->at(i)) < 3.)) max_pxeget = ntEgEt->at(i);
+         //if(eta_r == 1 && ntEgEt->at(i) > max_pxeget && ntCl_match->at(i) && (fabs(ntEgEta->at(i)) < 1.3)) max_pxeget = ntEgEt->at(i);
+         if(eta_r == 1 && ntEgEt->at(i) > max_pxeget && ntCl_match->at(i) && (fabs(ntEgEta->at(i)) < 0.8)) max_pxeget = ntEgEt->at(i);
+           
+      }// pxeg loop
+      
+      float max_pxeget_iso = -999.;
+      for(int i=0; i < ntnEg2; i++){
+         if(eta_r == 0 && ntEgEt->at(i) > max_pxeget && ((trigger_bit_width->at(i)&width_bit[0])==width_bit[0]) && (fabs(ntEgEta->at(i)) < 3.)) max_pxeget = ntEgEt->at(i);
+         //if(eta_r == 1 && ntEgEt->at(i) > max_pxeget && ntCl_match->at(i) && (fabs(ntEgEta->at(i)) < 1.3)) max_pxeget = ntEgEt->at(i);
+         if(eta_r == 1 && ntEgEt->at(i) > max_pxeget_iso && ntCl_iso_match->at(i) && (fabs(ntEgEta->at(i)) < 0.8)) max_pxeget_iso = ntEgEt->at(i);
+           
+      }// pxeg loop
+
+      hEG->Fill(max_eget);
+      if(max_pxeget > 0) hPXEG->Fill(max_pxeget);
+      if(max_pxeget_iso > 0) hPXIsoEG->Fill(max_pxeget_iso);
+   
+   } // event loop
+   
+   for (int i=0; i<= nbins; i++) {
+       hEG->SetBinContent(i, hEG -> Integral(i, nbins+1) );
+       hEG->SetBinError(i, sqrt( hEG -> GetBinContent(i) ) ); 
+
+       hPXEG->SetBinContent(i, hPXEG -> Integral(i, nbins+1) );
+       hPXEG->SetBinError(i, sqrt( hPXEG -> GetBinContent(i) ) ); 
+       
+       hPXIsoEG->SetBinContent(i, hPXIsoEG -> Integral(i, nbins+1) );
+       hPXIsoEG->SetBinError(i, sqrt( hPXIsoEG -> GetBinContent(i) ) ); 
+   }
+
+   cout << "0 bin: " << hEG->GetBinContent(0) << endl;
+   cout << "1 bin: " << hEG->GetBinContent(1) << endl;
+   cout << "11 bin: " << hEG->GetBinContent(11) * 30000./genN<< endl;
+   //cout << "11tk bin: " << hTKEG->GetBinContent(16) * 30000./genN<< endl;
+   //cout << "11tk iso bin: " << hTKIsoEG->GetBinContent(16) * 30000./genN<< endl;
+   cout << "11px bin: " << hPXEG->GetBinContent(11) * 30000./genN<< endl;
+   cout << "11px iso bin: " << hPXIsoEG->GetBinContent(11) * 30000./genN<< endl;
+   cout << "nbin bin: " << hEG->GetBinContent(nbins) << endl;
+   cout << "nbin+1 bin: " << hEG->GetBinContent(nbins+1) << endl;
+
+   hEG -> Scale(30000./genN);
+   hPXEG -> Scale(30000./genN);
+   hPXIsoEG -> Scale(30000./genN);
+
+   TCanvas *c1 = new TCanvas("c1","c1",800,700);
+   gStyle->SetOptStat(0);
+   gStyle->SetLineWidth(1); // axis width, default is 1
+   c1->SetTopMargin(0.05);
+   c1->SetBottomMargin(0.12);
+   c1->SetRightMargin(0.03);
+   c1->SetLeftMargin(0.2);
+   c1->SetLogy();
+   c1->SetGrid();
+   c1->SetTicky(1);
+   c1->SetTickx(1);
+
+   hEG->SetTitle("");
+   hEG->GetXaxis()->CenterTitle(true);
+   hEG->GetXaxis()->SetTitleOffset(1.);
+   hEG->GetXaxis()->SetTitleSize(0.055);
+   hEG->GetXaxis()->SetNdivisions(506);
+   hEG->GetYaxis()->SetNdivisions(546);
+   hEG->GetXaxis()->SetLabelSize(0.05);
+   hEG->GetYaxis()->SetLabelSize(0.05);
+   hEG->GetXaxis()->SetRangeUser(9.5,100.5);
+   hEG->GetYaxis()->SetRangeUser(1,35000);
+   hEG->GetYaxis()->SetTitleOffset(1.2);
+   hEG->GetYaxis()->SetTitleSize(0.055);
+   hEG->SetMinimum(0.1);
+
+   hEG->SetMarkerColor(2);
+   hEG->SetLineColor(2);
+   hEG->SetLineWidth(2);
+   hEG->SetMarkerStyle(20);
+   hEG->SetMarkerSize(1.);
+   hEG->Draw("HIST E");
+
+   hPXEG->SetMarkerColor(4);
+   hPXEG->SetLineColor(4);
+   hPXEG->SetLineWidth(2);
+   hPXEG->SetMarkerStyle(20);
+   hPXEG->SetMarkerSize(1.);
+   hPXEG->Draw("HIST same E");
+   
+   hPXIsoEG->SetMarkerColor(8);
+   hPXIsoEG->SetLineColor(8);
+   hPXIsoEG->SetLineWidth(2);
+   hPXIsoEG->SetMarkerStyle(20);
+   hPXIsoEG->SetMarkerSize(1.);
+   hPXIsoEG->Draw("HIST same E");
+
+   TLegend *Lgd = new TLegend(0.25, 0.8, 0.5, 0.95);
+   Lgd->SetFillColor(0);
+   Lgd->SetTextFont(42);
+   Lgd->SetTextSize(0.03);
+   Lgd->SetBorderSize(0);
+   Lgd->AddEntry(hEG,"Phase-2 L1 EG(crystal barrel and HGCAL endcap)","lp");
+   Lgd->AddEntry(hPXEG,"Pixel matching","lp");
+   Lgd->AddEntry(hPXIsoEG,"Pixel matching + Isolation","lp");
+   Lgd->Draw();
+
+   TLatex t(12,40000,"CMS Preliminary Simulation, Phase 2, <PU>=200");
+   t.SetTextSize(0.035);
+   t.Draw();
+
+   TString eta_str;
+
+   //if(eta_r == 0) eta_str = "#lbar#bf{#eta}#lbar < 1.5";
+   //if(eta_r == 1) eta_str = "#it{#lbar#bf{#eta}#lbar < 1.3}";
+   if(eta_r == 1) eta_str = "#it{#lbar#bf{#eta}#lbar < 0.8}";
+
+   TLatex eta_range(43,4500,eta_str);
+   eta_range.SetTextSize(0.05);
+   eta_range.Draw();
+
+
+   TString l1rate = "l1rate";
+
+   if(eta_r == 0) l1rate = l1rate + "_all.png";
+   if(eta_r == 1) l1rate = l1rate + "_r1.png";
+   if(eta_r == 2) l1rate = l1rate + "_r2.png";
+   if(eta_r == 3) l1rate = l1rate + "_r3.png";
+   if(eta_r == 4) l1rate = l1rate + "_r4.png";
+
+   c1->Print(l1rate);
+   
+   TCanvas *c2 = new TCanvas("c2","c2",700,700);
+   gStyle->SetOptStat(0);
+   gStyle->SetLineWidth(2); // axis width, default is 1
+   c2->SetTopMargin(0.07);
+   c2->SetBottomMargin(0.12);
+   c2->SetRightMargin(0.03);
+   c2->SetLeftMargin(0.2);
+   c2->SetLogy();
+   c2->SetGrid();
+   c2->SetTicky(1);
+   c2->SetTickx(1);
+
+   c2->cd();
+
+
+   r_PXEG->Add(hEG);
+   r_PXEG->Divide(hPXEG);
+
+   r_PXIsoEG->Add(hEG);
+   r_PXIsoEG->Divide(hPXIsoEG);
+
+   r_PXEG->SetTitle("");
+   r_PXEG->GetXaxis()->SetTitleOffset(1.3);
+   r_PXEG->GetXaxis()->SetTitleSize(0.045);
+   r_PXEG->GetXaxis()->SetNdivisions(505);
+   r_PXEG->GetYaxis()->SetNdivisions(546);
+   r_PXEG->GetXaxis()->SetLabelSize(0.05);
+   r_PXEG->GetYaxis()->SetLabelSize(0.05);
+   r_PXEG->GetXaxis()->SetRangeUser(10.5,60.5);
+   r_PXEG->GetYaxis()->SetRangeUser(1,60);
+   r_PXEG->GetYaxis()->SetTitleOffset(1.5);
+   r_PXEG->GetYaxis()->SetTitleSize(0.050);
+
+   r_PXEG->SetMarkerColor(1);
+   r_PXEG->SetLineColor(1);
+   r_PXEG->SetLineWidth(2);
+   r_PXEG->SetMarkerStyle(20);
+   r_PXEG->SetMarkerSize(1.);
+   
+   r_PXIsoEG->SetMarkerColor(4);
+   r_PXIsoEG->SetLineColor(4);
+   r_PXIsoEG->SetLineWidth(2);
+   r_PXIsoEG->SetMarkerStyle(20);
+   r_PXIsoEG->SetMarkerSize(1.);
+
+   r_PXEG->Draw("HIST e");
+   r_PXIsoEG->Draw("HIST same e");
+
+   cout << "11px bin: " << r_PXEG->GetBinContent(11) << " error: " << r_PXEG->GetBinError(11) << endl;
+   cout << "11px bin: " << r_PXIsoEG->GetBinContent(11) << " error: " << r_PXIsoEG->GetBinError(11) << endl;
+
+   TLatex t1(12,65,"CMS Preliminary Simulation, Phase 2, <PU>=200");
+   t1.SetTextSize(0.035);
+   t1.Draw();
+
+   TLatex eta_range1(12,45,eta_str);
+   eta_range1.SetTextSize(0.035);
+   eta_range1.Draw();
+
+   //TLegend *Lgd1 = new TLegend(0.25, 0.15, 0.65, 0.3);
+   TLegend *Lgd1 = new TLegend(0.25, 0.7, 0.65, 0.85);
+   Lgd1->SetFillColor(0);
+   Lgd1->SetTextFont(42);
+   Lgd1->SetTextSize(0.03);
+   Lgd1->SetBorderSize(1);
+   Lgd1->AddEntry(r_PXEG,"L1 Pixel Detector","lp");
+
+   Lgd1->Draw();
+
+   TString factor = "reduction_factor";
+
+   if(eta_r == 0) factor = factor + "_all.png";
+   if(eta_r == 1) factor = factor + "_r1.png";
+   if(eta_r == 2) factor = factor + "_r2.png";
+   if(eta_r == 3) factor = factor + "_r3.png";  
+   if(eta_r == 4) factor = factor + "_r4.png";
+
+   c2->Print(factor);   
+
+   TFile *output = new TFile("rate-reduction.root","RECREATE");
+   c1->Write();
+   c2->Write();
+   hEG->Write();
+   hPXEG->Write();
+   hPXIsoEG->Write();
+   Lgd->Write();
+   t.Write();
+   eta_range.Write();
+
+   r_PXEG->Write();
+   r_PXIsoEG->Write();
+
+   output->Close();
+
+
+}
---
+++ CMSSW9/TrkIso/TrkIso-MinBias-Draw/l1Rate_my.h
@@ -0,0 +1,331 @@
+//////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Wed Sep  5 00:26:47 2018 by ROOT version 6.12/06
+// from TTree t/t
+// found on file: Tree_MinBias_PU200_modified.root
+//////////////////////////////////////////////////////////
+
+#ifndef l1Rate_my_h
+#define l1Rate_my_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+
+// Header file for the classes stored in the TTree if any.
+#include "vector"
+#include "vector"
+#include "vector"
+
+class l1Rate_my {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           totalEvent;
+   Float_t         totalEgN;
+   Int_t           ntnEg2;
+   Int_t           event_with_selection_passed;
+   Int_t           event_with_trigger_passed;
+   Int_t           L1TkEleN;
+   Int_t           L1TkEleIsoN;
+   vector<float>   *ntEgEt;
+   vector<float>   *ntL1TkEleEt;
+   vector<float>   *ntL1TkEleIsoEt;
+   vector<float>   *ntEgEta;
+   vector<bool>    *ntclusterIsEG;
+   vector<float>   *ntL1TkEleEta;
+   vector<float>   *ntL1TkEleIsoEta;
+   vector<float>   *ntL1TkElePhi;
+   vector<float>   *ntL1TkEleIsoPhi;
+   vector<bool>    *ntCl_match;
+   vector<bool>    *ntCl_iso_match;
+   vector<bool>    *withoutEM_match;
+   vector<bool>    *withEM_match;
+   vector<bool>    *isTrack_match;
+   vector<float>   *chi2;
+   vector<float>   *track_dr;
+   vector<bool>    *ntCl_match_wo4thPix;
+   vector<bool>    *ntCl_match_wo3thPix;
+   vector<bool>    *ntCl_match_wo2thPix;
+   vector<bool>    *ntCl_match_wo1thPix;
+   vector<int>     *Npass_woEM_wo4thPix;
+   vector<int>     *Npass_woEM_wo3thPix;
+   vector<int>     *Npass_woEM_wo2thPix;
+   vector<int>     *Npass_woEM_wo1thPix;
+   vector<int>     *Npass_wEM_wo4thPix;
+   vector<int>     *Npass_wEM_wo3thPix;
+   vector<int>     *Npass_wEM_wo2thPix;
+   vector<int>     *Npass_wEM_wo1thPix;
+   vector<float>   *deta_L12EM_wo4thPix;
+   vector<float>   *deta_L12EM_wo3thPix;
+   vector<float>   *deta_L12EM_wo2thPix;
+   vector<float>   *deta_L12EM_wo1thPix;
+   vector<float>   *dphi_L12EM_wo4thPix;
+   vector<float>   *dphi_L12EM_wo3thPix;
+   vector<float>   *dphi_L12EM_wo2thPix;
+   vector<float>   *dphi_L12EM_wo1thPix;
+   vector<int>     *pass_Ele;
+   vector<int>     *pass_Pos;
+   vector<int>     *pass_ElePos;
+   vector<int>     *ntfirstPix;
+   vector<int>     *ntsecondPix;
+   vector<int>     *ntthirdPix;
+   vector<int>     *ntfourthPix;
+   vector<int>     *trigger_bit_width;
+   vector<int>     *trigger_4Hits_bit_width;
+
+   // List of branches
+   TBranch        *b_count_Entry;   //!
+   TBranch        *b_EgN;   //!
+   TBranch        *b_ntnEg2;   //!
+   TBranch        *b_event_denominator;   //!
+   TBranch        *b_event_nominator;   //!
+   TBranch        *b_L1TkEleN;   //!
+   TBranch        *b_L1TkEleIsoN;   //!
+   TBranch        *b_ntEgEt;   //!
+   TBranch        *b_ntL1TkEleEt;   //!
+   TBranch        *b_ntL1TkEleIsoEt;   //!
+   TBranch        *b_ntEgEta;   //!
+   TBranch        *b_ntclusterIsEG;   //!
+   TBranch        *b_ntL1TkEleEta;   //!
+   TBranch        *b_ntL1TkEleIsoEta;   //!
+   TBranch        *b_ntL1TkElePhi;   //!
+   TBranch        *b_ntL1TkEleIsoPhi;   //!
+   TBranch        *b_ntCl_match;   //!
+   TBranch        *b_ntCl_iso_match;   //!
+   TBranch        *b_withoutEM_match;   //!
+   TBranch        *b_withEM_match;   //!
+   TBranch        *b_isTrack_match;   //!
+   TBranch        *b_chi2;   //!
+   TBranch        *b_track_dr;   //!
+   TBranch        *b_ntCl_match_wo4thPix;   //!
+   TBranch        *b_ntCl_match_wo3thPix;   //!
+   TBranch        *b_ntCl_match_wo2thPix;   //!
+   TBranch        *b_ntCl_match_wo1thPix;   //!
+   TBranch        *b_Npass_woEM_wo4thPix;   //!
+   TBranch        *b_Npass_woEM_wo3thPix;   //!
+   TBranch        *b_Npass_woEM_wo2thPix;   //!
+   TBranch        *b_Npass_woEM_wo1thPix;   //!
+   TBranch        *b_Npass_wEM_wo4thPix;   //!
+   TBranch        *b_Npass_wEM_wo3thPix;   //!
+   TBranch        *b_Npass_wEM_wo2thPix;   //!
+   TBranch        *b_Npass_wEM_wo1thPix;   //!
+   TBranch        *b_deta_L12EM_wo4thPix;   //!
+   TBranch        *b_deta_L12EM_wo3thPix;   //!
+   TBranch        *b_deta_L12EM_wo2thPix;   //!
+   TBranch        *b_deta_L12EM_wo1thPix;   //!
+   TBranch        *b_dphi_L12EM_wo4thPix;   //!
+   TBranch        *b_dphi_L12EM_wo3thPix;   //!
+   TBranch        *b_dphi_L12EM_wo2thPix;   //!
+   TBranch        *b_dphi_L12EM_wo1thPix;   //!
+   TBranch        *b_pass_Ele;   //!
+   TBranch        *b_pass_Pos;   //!
+   TBranch        *b_pass_ElePos;   //!
+   TBranch        *b_ntfirstPix;   //!
+   TBranch        *b_ntsecondPix;   //!
+   TBranch        *b_ntthirdPix;   //!
+   TBranch        *b_ntfourthPix;   //!
+   TBranch        *b_trigger_bit_width;   //!
+   TBranch        *b_trigger_4Hits_bit_width;   //!
+
+   l1Rate_my(TTree *tree=0);
+   virtual ~l1Rate_my();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+};
+
+#endif
+
+#ifdef l1Rate_my_cxx
+l1Rate_my::l1Rate_my(TTree *tree) : fChain(0) 
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0) {
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("Tree_MinBias_PU200_modified.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("Tree_MinBias_PU200_modified.root");
+      }
+      f->GetObject("t",tree);
+
+   }
+   Init(tree);
+}
+
+l1Rate_my::~l1Rate_my()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t l1Rate_my::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t l1Rate_my::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void l1Rate_my::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set object pointer
+   ntEgEt = 0;
+   ntL1TkEleEt = 0;
+   ntL1TkEleIsoEt = 0;
+   ntEgEta = 0;
+   ntclusterIsEG = 0;
+   ntL1TkEleEta = 0;
+   ntL1TkEleIsoEta = 0;
+   ntL1TkElePhi = 0;
+   ntL1TkEleIsoPhi = 0;
+   ntCl_match = 0;
+   ntCl_iso_match = 0;
+   withoutEM_match = 0;
+   withEM_match = 0;
+   isTrack_match = 0;
+   chi2 = 0;
+   track_dr = 0;
+   ntCl_match_wo4thPix = 0;
+   ntCl_match_wo3thPix = 0;
+   ntCl_match_wo2thPix = 0;
+   ntCl_match_wo1thPix = 0;
+   Npass_woEM_wo4thPix = 0;
+   Npass_woEM_wo3thPix = 0;
+   Npass_woEM_wo2thPix = 0;
+   Npass_woEM_wo1thPix = 0;
+   Npass_wEM_wo4thPix = 0;
+   Npass_wEM_wo3thPix = 0;
+   Npass_wEM_wo2thPix = 0;
+   Npass_wEM_wo1thPix = 0;
+   deta_L12EM_wo4thPix = 0;
+   deta_L12EM_wo3thPix = 0;
+   deta_L12EM_wo2thPix = 0;
+   deta_L12EM_wo1thPix = 0;
+   dphi_L12EM_wo4thPix = 0;
+   dphi_L12EM_wo3thPix = 0;
+   dphi_L12EM_wo2thPix = 0;
+   dphi_L12EM_wo1thPix = 0;
+   pass_Ele = 0;
+   pass_Pos = 0;
+   pass_ElePos = 0;
+   ntfirstPix = 0;
+   ntsecondPix = 0;
+   ntthirdPix = 0;
+   ntfourthPix = 0;
+   trigger_bit_width = 0;
+   trigger_4Hits_bit_width = 0;
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("totalEvent", &totalEvent, &b_count_Entry);
+   fChain->SetBranchAddress("totalEgN", &totalEgN, &b_EgN);
+   fChain->SetBranchAddress("ntnEg2", &ntnEg2, &b_ntnEg2);
+   fChain->SetBranchAddress("event_with_selection_passed", &event_with_selection_passed, &b_event_denominator);
+   fChain->SetBranchAddress("event_with_trigger_passed", &event_with_trigger_passed, &b_event_nominator);
+   fChain->SetBranchAddress("L1TkEleN", &L1TkEleN, &b_L1TkEleN);
+   fChain->SetBranchAddress("L1TkEleIsoN", &L1TkEleIsoN, &b_L1TkEleIsoN);
+   fChain->SetBranchAddress("ntEgEt", &ntEgEt, &b_ntEgEt);
+   fChain->SetBranchAddress("ntL1TkEleEt", &ntL1TkEleEt, &b_ntL1TkEleEt);
+   fChain->SetBranchAddress("ntL1TkEleIsoEt", &ntL1TkEleIsoEt, &b_ntL1TkEleIsoEt);
+   fChain->SetBranchAddress("ntEgEta", &ntEgEta, &b_ntEgEta);
+   fChain->SetBranchAddress("ntclusterIsEG", &ntclusterIsEG, &b_ntclusterIsEG);
+   fChain->SetBranchAddress("ntL1TkEleEta", &ntL1TkEleEta, &b_ntL1TkEleEta);
+   fChain->SetBranchAddress("ntL1TkEleIsoEta", &ntL1TkEleIsoEta, &b_ntL1TkEleIsoEta);
+   fChain->SetBranchAddress("ntL1TkElePhi", &ntL1TkElePhi, &b_ntL1TkElePhi);
+   fChain->SetBranchAddress("ntL1TkEleIsoPhi", &ntL1TkEleIsoPhi, &b_ntL1TkEleIsoPhi);
+   fChain->SetBranchAddress("ntCl_match", &ntCl_match, &b_ntCl_match);
+   fChain->SetBranchAddress("ntCl_iso_match", &ntCl_iso_match, &b_ntCl_iso_match);
+   fChain->SetBranchAddress("withoutEM_match", &withoutEM_match, &b_withoutEM_match);
+   fChain->SetBranchAddress("withEM_match", &withEM_match, &b_withEM_match);
+   fChain->SetBranchAddress("isTrack_match", &isTrack_match, &b_isTrack_match);
+   fChain->SetBranchAddress("chi2", &chi2, &b_chi2);
+   fChain->SetBranchAddress("track_dr", &track_dr, &b_track_dr);
+   fChain->SetBranchAddress("ntCl_match_wo4thPix", &ntCl_match_wo4thPix, &b_ntCl_match_wo4thPix);
+   fChain->SetBranchAddress("ntCl_match_wo3thPix", &ntCl_match_wo3thPix, &b_ntCl_match_wo3thPix);
+   fChain->SetBranchAddress("ntCl_match_wo2thPix", &ntCl_match_wo2thPix, &b_ntCl_match_wo2thPix);
+   fChain->SetBranchAddress("ntCl_match_wo1thPix", &ntCl_match_wo1thPix, &b_ntCl_match_wo1thPix);
+   fChain->SetBranchAddress("Npass_woEM_wo4thPix", &Npass_woEM_wo4thPix, &b_Npass_woEM_wo4thPix);
+   fChain->SetBranchAddress("Npass_woEM_wo3thPix", &Npass_woEM_wo3thPix, &b_Npass_woEM_wo3thPix);
+   fChain->SetBranchAddress("Npass_woEM_wo2thPix", &Npass_woEM_wo2thPix, &b_Npass_woEM_wo2thPix);
+   fChain->SetBranchAddress("Npass_woEM_wo1thPix", &Npass_woEM_wo1thPix, &b_Npass_woEM_wo1thPix);
+   fChain->SetBranchAddress("Npass_wEM_wo4thPix", &Npass_wEM_wo4thPix, &b_Npass_wEM_wo4thPix);
+   fChain->SetBranchAddress("Npass_wEM_wo3thPix", &Npass_wEM_wo3thPix, &b_Npass_wEM_wo3thPix);
+   fChain->SetBranchAddress("Npass_wEM_wo2thPix", &Npass_wEM_wo2thPix, &b_Npass_wEM_wo2thPix);
+   fChain->SetBranchAddress("Npass_wEM_wo1thPix", &Npass_wEM_wo1thPix, &b_Npass_wEM_wo1thPix);
+   fChain->SetBranchAddress("deta_L12EM_wo4thPix", &deta_L12EM_wo4thPix, &b_deta_L12EM_wo4thPix);
+   fChain->SetBranchAddress("deta_L12EM_wo3thPix", &deta_L12EM_wo3thPix, &b_deta_L12EM_wo3thPix);
+   fChain->SetBranchAddress("deta_L12EM_wo2thPix", &deta_L12EM_wo2thPix, &b_deta_L12EM_wo2thPix);
+   fChain->SetBranchAddress("deta_L12EM_wo1thPix", &deta_L12EM_wo1thPix, &b_deta_L12EM_wo1thPix);
+   fChain->SetBranchAddress("dphi_L12EM_wo4thPix", &dphi_L12EM_wo4thPix, &b_dphi_L12EM_wo4thPix);
+   fChain->SetBranchAddress("dphi_L12EM_wo3thPix", &dphi_L12EM_wo3thPix, &b_dphi_L12EM_wo3thPix);
+   fChain->SetBranchAddress("dphi_L12EM_wo2thPix", &dphi_L12EM_wo2thPix, &b_dphi_L12EM_wo2thPix);
+   fChain->SetBranchAddress("dphi_L12EM_wo1thPix", &dphi_L12EM_wo1thPix, &b_dphi_L12EM_wo1thPix);
+   fChain->SetBranchAddress("pass_Ele", &pass_Ele, &b_pass_Ele);
+   fChain->SetBranchAddress("pass_Pos", &pass_Pos, &b_pass_Pos);
+   fChain->SetBranchAddress("pass_ElePos", &pass_ElePos, &b_pass_ElePos);
+   fChain->SetBranchAddress("ntfirstPix", &ntfirstPix, &b_ntfirstPix);
+   fChain->SetBranchAddress("ntsecondPix", &ntsecondPix, &b_ntsecondPix);
+   fChain->SetBranchAddress("ntthirdPix", &ntthirdPix, &b_ntthirdPix);
+   fChain->SetBranchAddress("ntfourthPix", &ntfourthPix, &b_ntfourthPix);
+   fChain->SetBranchAddress("trigger_bit_width", &trigger_bit_width, &b_trigger_bit_width);
+   fChain->SetBranchAddress("trigger_4Hits_bit_width", &trigger_4Hits_bit_width, &b_trigger_4Hits_bit_width);
+   Notify();
+}
+
+Bool_t l1Rate_my::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+   return kTRUE;
+}
+
+void l1Rate_my::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t l1Rate_my::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef l1Rate_my_cxx
