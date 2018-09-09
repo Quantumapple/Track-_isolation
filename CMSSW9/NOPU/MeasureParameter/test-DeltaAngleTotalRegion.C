8f1f97e2f5264c8940b95d19d66bddc52a634073
Quantumapple <jongho.lee@cern.ch>
2018-09-09 23:20:46 +0900

    Upgrade version

--- CMSSW9/NOPU/MeasureParameter/test-DeltaAngleTotalRegion.C
+++ CMSSW9/NOPU/MeasureParameter/test-DeltaAngleTotalRegion.C
@@ -38,39 +38,46 @@ void test::Loop()
    TString file_name;
    
    // Histograms for DeltaEta(pixel, pixel)
-   TH1F *h1 = new TH1F("dEtaL123pp1","L_{1}L_{2}-L_{1}L_{3}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *h2 = new TH1F("dEtaL123pp2","L_{1}L_{2}-L_{2}L_{3}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *h3 = new TH1F("dEtaL123pp3","L_{1}L_{3}-L_{2}L_{3}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *h1 = new TH1F("dEtaCase1pp1","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *h2 = new TH1F("dEtaCase1pp2","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *h3 = new TH1F("dEtaCase1pp3","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
    
-   TH1F *a1 = new TH1F("dEtaL12D1pp1","L_{1}L_{2}-L_{1}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *a2 = new TH1F("dEtaL12D1pp2","L_{1}L_{2}-L_{2}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *a3 = new TH1F("dEtaL12D1pp3","L_{1}D_{1}-L_{2}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *a1 = new TH1F("dEtaCase2pp1","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *a2 = new TH1F("dEtaCase2pp2","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *a3 = new TH1F("dEtaCase2pp3","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
    
-   TH1F *b1 = new TH1F("dEtaL13D1pp1","L_{1}L_{3}-L_{1}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *b2 = new TH1F("dEtaL13D1pp2","L_{1}L_{3}-L_{3}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *b3 = new TH1F("dEtaL13D1pp3","L_{1}D_{1}-L_{3}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *b1 = new TH1F("dEtaCase3pp1","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *b2 = new TH1F("dEtaCase3pp2","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *b3 = new TH1F("dEtaCase3pp3","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
 
-   TH1F *c1 = new TH1F("dEtaL23D1pp1","L_{2}L_{3}-L_{2}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *c2 = new TH1F("dEtaL23D1pp2","L_{2}L_{3}-L_{3}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *c3 = new TH1F("dEtaL23D1pp3","L_{2}D_{1}-L_{3}D_{1}; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *c1 = new TH1F("dEtaCase4pp1","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *c2 = new TH1F("dEtaCase4pp2","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *c3 = new TH1F("dEtaCase4pp3","; #Delta#eta (pixel,pixel); Number of combinations",1000,-0.01,0.01);
    
    // Histograms for DeltaEta(pv, pixel)
-   TH1F *hh1 = new TH1F("dEtaL123PV1","PVL3-PVL2; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *hh2 = new TH1F("dEtaL123PV2","PVL3-PVL1; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *hh1 = new TH1F("dEta1STPV1","; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *hh2 = new TH1F("dEta2NDPV2","; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
    
-   TH1F *aa1 = new TH1F("dEtaL12D1PV1","PVD1-PVL2; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
-   TH1F *aa2 = new TH1F("dEtaL12D1PV2","PVD1-PVL1; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *aa1 = new TH1F("dEta3RDPV1","; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *aa2 = new TH1F("dEta4THPV2","; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
    
-   TH1F *bb1 = new TH1F("dEtaL13D1PV1","PVD1-PVL3; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
+   TH1F *bb1 = new TH1F("dEta5THPV1","; #Delta#eta (PV,pixel); Number of combinations",1000,-0.01,0.01);
    
    // Histograms for DeltaPhi difference
-   TH1F *hhh1 = new TH1F("dPhiL123","#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2L3)]; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
-   TH1F *aaa1 = new TH1F("dPhiL12D1","#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2D1)]; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
-   TH1F *bbb1 = new TH1F("dPhiL13D1","#Delta#phi[#phi(PVL1)-#phi(L1L3)]-#Delta#phi[#phi(L1L3)-#phi(L3D1)]; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
-   TH1F *ccc1 = new TH1F("dPhiL23D1","#Delta#phi[#phi(PVL2)-#phi(L2L3)]-#Delta#phi[#phi(L2L3)-#phi(L3D1)]; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
+   TH1F *hhh1 = new TH1F("dPhi1ST","; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
+   TH1F *aaa1 = new TH1F("dPhi2ND","; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
+   TH1F *bbb1 = new TH1F("dPhi3RD","; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
+   TH1F *ccc1 = new TH1F("dPhi4TH","; #Delta#phi_{i}-#Delta#phi_{j}; Number of combinations",1000,-0.01,0.01);
+
+   Bool_t eta_flag1 = false;
+   Bool_t eta_flag2 = false;
+   Bool_t eta_flag3 = false;
+   Bool_t eta_flag4 = false;
+   Bool_t eta_flag5 = false;
+   Bool_t eta_flag6 = false;
 
    //for (Long64_t jentry=0; jentry<nentries;jentry++) {
-   for (Long64_t jentry=0; jentry<100000;jentry++) {
+   for (Long64_t jentry=0; jentry<1000;jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
@@ -107,20 +114,20 @@ void test::Loop()
       //if( fabs(propGenEta) > 1.8 && fabs(propGenEta) < 2.7 ) eta_region = 4;
       //if( fabs(propGenEta) > 2.7 && fabs(propGenEta) < 2.9 ) eta_region = 5;
       //if( fabs(propGenEta) > 2.9 && fabs(propGenEta) < 3.0 ) eta_region = 6;
-      //if( fabs(propGenEta) > 3.0 ) continue;
+      if( fabs(propGenEta) > 3.0 ) continue;
       
-      if( eta_region != 2 ) continue;
-      //if( eta_region == 1 ) if( bRecHitN == 0 ) continue;
+      //if( eta_region != 2 ) continue;
+      if( eta_region == 1 ) if( bRecHitN == 0 ) continue;
       if( eta_region == 2 || eta_region == 3 ) if( bRecHitN == 0 || fRecHitN == 0 ) continue;
-      //if( eta_region > 3 ) if( fRecHitN == 0 ) continue;
+      if( eta_region > 3 ) if( fRecHitN == 0 ) continue;
       cout << "Process: " << jentry << endl;
 
-      if( eta_region == 1 ) file_name = "dAngleEtaRegion1.root";   
-      if( eta_region == 2 ) file_name = "dAngleEtaRegion2.root";
-      if( eta_region == 3 ) file_name = "dAngleEtaRegion3.root";
-      if( eta_region == 4 ) file_name = "dAngleEtaRegion4.root";
-      if( eta_region == 5 ) file_name = "dAngleEtaRegion5.root";
-      if( eta_region == 6 ) file_name = "dAngleEtaRegion6.root";
+      if( eta_region == 1 ) { file_name = "dAngleEtaRegion1.root"; eta_flag1 = true; }   
+      if( eta_region == 2 ) { file_name = "dAngleEtaRegion2.root"; eta_flag2 = true; }
+      if( eta_region == 3 ) { file_name = "dAngleEtaRegion3.root"; eta_flag3 = true; }
+      if( eta_region == 4 ) { file_name = "dAngleEtaRegion4.root"; eta_flag4 = true; }
+      if( eta_region == 5 ) { file_name = "dAngleEtaRegion5.root"; eta_flag5 = true; }
+      if( eta_region == 6 ) { file_name = "dAngleEtaRegion6.root"; eta_flag6 = true; }
     
       // Store pixel clusters w.r.t eta range
       StorePixelHits(eta_region, propGenPhi, propGenEta);
@@ -194,7 +201,158 @@ void test::Loop()
    } // event loop
    
    TFile *output = new TFile(file_name,"RECREATE");
+  
+   if( eta_flag1 )
+   {
+      h1->SetTitle("L_{1}L_{2}-L_{1}L_{3}");
+      h2->SetTitle("L_{1}L_{2}-L_{2}L_{3}");
+      h3->SetTitle("L_{1}L_{3}-L_{2}L_{3}");
+      hh1->SetTitle("PVL3-PVL2");
+      hh2->SetTitle("PVL3-PVL1");
+      hhh1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2L3)]");
+   }
+
+   if( eta_flag2 )
+   {
+      h1->SetTitle("L_{1}L_{2}-L_{1}L_{3}");
+      h2->SetTitle("L_{1}L_{2}-L_{2}L_{3}");
+      h3->SetTitle("L_{1}L_{3}-L_{2}L_{3}");
+      hh1->SetTitle("PVL3-PVL2");
+      hh2->SetTitle("PVL3-PVL1");
+      hhh1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2L3)]");
+      
+      a1->SetTitle("L_{1}L_{2}-L_{1}D_{1}"); 
+      a2->SetTitle("L_{1}L_{2}-L_{2}D_{1}"); 
+      a3->SetTitle("L_{1}D_{1}-L_{2}D_{1}"); 
+      aa1->SetTitle("PVD1-PVL2");
+      aa2->SetTitle("PVD1-PVL1");
+      aaa1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2D1)]");
+      
+      b1->SetTitle("L_{1}L_{3}-L_{1}D_{1}");
+      b2->SetTitle("L_{1}L_{3}-L_{3}D_{1}");
+      b3->SetTitle("L_{1}D_{1}-L_{3}D_{1}");
+      bb1->SetTitle("PVD1-PVL3");
+      bbb1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L3)]-#Delta#phi[#phi(L1L3)-#phi(L3D1)]");
+      
+      c1->SetTitle("L_{2}L_{3}-L_{2}D_{1}");
+      c2->SetTitle("L_{2}L_{3}-L_{3}D_{1}");
+      c3->SetTitle("L_{2}D_{1}-L_{3}D_{1}");
+      ccc1->SetTitle("#Delta#phi[#phi(PVL2)-#phi(L2L3)]-#Delta#phi[#phi(L2L3)-#phi(L3D1)]");
+   }
    
+   if( eta_flag3 )
+   {
+      h1->SetTitle("L_{1}L_{2}-L_{1}D_{1}");
+      h2->SetTitle("L_{1}L_{2}-L_{2}D_{1}");
+      h3->SetTitle("L_{1}D_{1}-L_{2}D_{1}");
+      hh1->SetTitle("PVD1-PVL2");
+      hh2->SetTitle("PVD1-PVL1");
+      hhh1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2D1)]");
+      
+      a1->SetTitle("L_{1}L_{2}-L_{1}D_{2}"); 
+      a2->SetTitle("L_{1}L_{2}-L_{2}D_{2}"); 
+      a3->SetTitle("L_{1}D_{2}-L_{2}D_{2}"); 
+      aa1->SetTitle("PVD2-PVL2");
+      aa2->SetTitle("PVD2-PVL1");
+      aaa1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1L2)]-#Delta#phi[#phi(L1L2)-#phi(L2D2)]");
+      
+      b1->SetTitle("L_{1}D_{1}-L_{1}D_{2}");
+      b2->SetTitle("L_{1}D_{1}-D_{1}D_{2}");
+      b3->SetTitle("L_{1}D_{2}-D_{1}D_{2}");
+      bb1->SetTitle("PVD1-PVD1");
+      bbb1->SetTitle("#Delta#phi[#phi(PVL1)-#phi(L1D1)]-#Delta#phi[#phi(L1D1)-#phi(D1D2)]");
+      
+      c1->SetTitle("L_{2}D_{1}-L_{2}D_{2}");
+      c2->SetTitle("L_{2}D_{1}-D_{1}D_{1}");
+      c3->SetTitle("L_{2}D_{2}-D_{1}D_{2}");
+      ccc1->SetTitle("#Delta#phi[#phi(PVL2)-#phi(L2D1)]-#Delta#phi[#phi(L2D1)-#phi(D1D2)]");
+   }
+
+   if( eta_flag4 )
+   {
+      h1->SetTitle("D_{1}D_{2}-D_{1}D_{3}");
+      h2->SetTitle("D_{1}D_{2}-D_{2}D_{3}");
+      h3->SetTitle("D_{1}D_{3}-D_{2}D_{3}");
+      hh1->SetTitle("PVD3-PVD2");
+      hh2->SetTitle("PVD3-PVD1");
+      hhh1->SetTitle("#Delta#phi[#phi(PVD1)-#phi(D1D2)]-#Delta#phi[#phi(D1D2)-#phi(D2D3)]");
+      
+      a1->SetTitle("D_{1}D_{2}-D_{1}D_{4}"); 
+      a2->SetTitle("D_{1}D_{2}-D_{2}D_{4}"); 
+      a3->SetTitle("D_{1}D_{4}-D_{2}D_{4}"); 
+      aa1->SetTitle("PVD4-PVD2");
+      aa2->SetTitle("PVD4-PVD1");
+      aaa1->SetTitle("#Delta#phi[#phi(PVD1)-#phi(D1D2)]-#Delta#phi[#phi(D1D2)-#phi(D2D4)]");
+      
+      b1->SetTitle("D_{1}D_{3}-D_{1}D_{4}");
+      b2->SetTitle("D_{1}D_{3}-D_{3}D_{4}");
+      b3->SetTitle("D_{1}D_{4}-D_{3}D_{4}");
+      bb1->SetTitle("PVD4-PVD3");
+      bbb1->SetTitle("#Delta#phi[#phi(PVD1)-#phi(D1D3)]-#Delta#phi[#phi(D1D3)-#phi(D3D4)]");
+      
+      c1->SetTitle("D_{2}D_{3}-D_{2}D_{4}");
+      c2->SetTitle("D_{2}D_{3}-D_{3}D_{4}");
+      c3->SetTitle("D_{2}D_{4}-D_{3}D_{4}");
+      ccc1->SetTitle("#Delta#phi[#phi(PVD2)-#phi(D2D3)]-#Delta#phi[#phi(D2D3)-#phi(D3D4)]");
+   }
+   
+   if( eta_flag5 )
+   {
+      h1->SetTitle("D_{2}D_{3}-D_{2}D_{4}");
+      h2->SetTitle("D_{2}D_{3}-D_{3}D_{4}");
+      h3->SetTitle("D_{2}D_{4}-D_{3}D_{4}");
+      hh1->SetTitle("PVD4-PVD3");
+      hh2->SetTitle("PVD4-PVD2");
+      hhh1->SetTitle("#Delta#phi[#phi(PVD2)-#phi(D2D3)]-#Delta#phi[#phi(D2D3)-#phi(D3D4)]");
+      
+      a1->SetTitle("D_{2}D_{3}-D_{2}D_{5}"); 
+      a2->SetTitle("D_{2}D_{3}-D_{3}D_{5}"); 
+      a3->SetTitle("D_{2}D_{5}-D_{3}D_{5}"); 
+      aa1->SetTitle("PVD5-PVD3");
+      aa2->SetTitle("PVD5-PVD2");
+      aaa1->SetTitle("#Delta#phi[#phi(PVD2)-#phi(D2D3)]-#Delta#phi[#phi(D2D3)-#phi(D3D5)]");
+      
+      b1->SetTitle("D_{2}D_{4}-D_{2}D_{5}");
+      b2->SetTitle("D_{2}D_{4}-D_{4}D_{5}");
+      b3->SetTitle("D_{2}D_{5}-D_{4}D_{5}");
+      bb1->SetTitle("PVD5-PVD4");
+      bbb1->SetTitle("#Delta#phi[#phi(PVD2)-#phi(D2D4)]-#Delta#phi[#phi(D2D4)-#phi(D4D5)]");
+      
+      c1->SetTitle("D_{3}D_{4}-D_{3}D_{5}");
+      c2->SetTitle("D_{3}D_{4}-D_{4}D_{5}");
+      c3->SetTitle("D_{3}D_{5}-D_{4}D_{5}");
+      ccc1->SetTitle("#Delta#phi[#phi(PVD3)-#phi(D3D4)]-#Delta#phi[#phi(D3D4)-#phi(D4D5)]");
+   }
+   
+   if( eta_flag6 )
+   {
+      h1->SetTitle("D_{3}D_{4}-D_{3}D_{5}");
+      h2->SetTitle("D_{3}D_{4}-D_{4}D_{5}");
+      h3->SetTitle("D_{3}D_{5}-D_{4}D_{5}");
+      hh1->SetTitle("PVD5-PVD4");
+      hh2->SetTitle("PVD5-PVD3");
+      hhh1->SetTitle("#Delta#phi[#phi(PVD3)-#phi(D3D4)]-#Delta#phi[#phi(D3D4)-#phi(D4D5)]");
+      
+      a1->SetTitle("D_{3}D_{4}-D_{3}D_{6}"); 
+      a2->SetTitle("D_{3}D_{4}-D_{4}D_{6}"); 
+      a3->SetTitle("D_{3}D_{6}-D_{4}D_{6}"); 
+      aa1->SetTitle("PVD6-PVD4");
+      aa2->SetTitle("PVD6-PVD3");
+      aaa1->SetTitle("#Delta#phi[#phi(PVD3)-#phi(D3D4)]-#Delta#phi[#phi(D3D4)-#phi(D4D6)]");
+      
+      b1->SetTitle("D_{3}D_{5}-D_{3}D_{6}");
+      b2->SetTitle("D_{3}D_{5}-D_{5}D_{6}");
+      b3->SetTitle("D_{3}D_{6}-D_{5}D_{6}");
+      bb1->SetTitle("PVD6-PVD5");
+      bbb1->SetTitle("#Delta#phi[#phi(PVD3)-#phi(D3D5)]-#Delta#phi[#phi(D3D5)-#phi(D5D6)]");
+      
+      c1->SetTitle("D_{4}D_{5}-D_{4}D_{6}");
+      c2->SetTitle("D_{4}D_{5}-D_{5}D_{6}");
+      c3->SetTitle("D_{4}D_{6}-D_{5}D_{6}");
+      ccc1->SetTitle("#Delta#phi[#phi(PVD4)-#phi(D4D5)]-#Delta#phi[#phi(D4D5)-#phi(D5D6)]");
+   }
+
+
    h1->GetXaxis()->CenterTitle(true);
    h1->GetXaxis()->SetTitleSize(0.05);
    h1->GetYaxis()->SetTitleSize(0.04);
