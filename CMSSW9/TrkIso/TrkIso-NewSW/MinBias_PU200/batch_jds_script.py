ffef2e58ca234d76a172c262c3739b02a4dff512
jongho <jongho.lee@cern.ch>
2018-10-12 20:27:10 +0900

    Fix for condor

--- CMSSW9/TrkIso/TrkIso-NewSW/MinBias_PU200/batch_jds_script.py
+++ CMSSW9/TrkIso/TrkIso-NewSW/MinBias_PU200/batch_jds_script.py
@@ -7,9 +7,9 @@ def makeBatchConfigjdsFile(job_dir):
     config+='transfer_input_files = x_test.C \n'
     config+='should_transfer_files = YES \n'
     config+='when_to_transfer_output = ON_EXIT \n'
-    config+='requirements = ( HasSingularity == true) \n'
-    config+='+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el6:latest"  \n'
-    config+='+SingularityBind = "/cvmfs, /cms, /share" \n'
+    #config+='requirements = ( HasSingularity == true) \n'
+    #config+='+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el6:latest"  \n'
+    #config+='+SingularityBind = "/cvmfs, /cms, /share" \n'
     config+='queue'
 
     return config
