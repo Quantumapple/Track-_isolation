61487c599b62bb3a51d4269bc8f8e598081f294f
jongho <jongho.lee@cern.ch>
2018-12-03 17:30:41 +0900

    New option to run Kisti

--- CMSSW9/TrkIso/TrkIso-NewSW/MinBias_PU200/batch_jds_script.py
+++ CMSSW9/TrkIso/TrkIso-NewSW/MinBias_PU200/batch_jds_script.py
@@ -7,9 +7,10 @@ def makeBatchConfigjdsFile(job_dir):
     config+='transfer_input_files = x_test.C \n'
     config+='should_transfer_files = YES \n'
     config+='when_to_transfer_output = ON_EXIT \n'
-    #config+='requirements = ( HasSingularity == true) \n'
-    #config+='+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el6:latest"  \n'
-    #config+='+SingularityBind = "/cvmfs, /cms, /share" \n'
+    config+='requirements = ( HasSingularity == true) \n'
+    config+='accounting_group=group_cms \n'
+    config+='+SingularityImage = "/cvmfs/singularity.opensciencegrid.org/opensciencegrid/osgvo-el6:latest"  \n'
+    config+='+SingularityBind = "/cvmfs, /cms, /share" \n'
     config+='queue'
 
     return config
